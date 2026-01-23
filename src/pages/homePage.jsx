dd22d3518b2761a1f87b86d6b61c8fca9e2cb604
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-01-23 08:21:44 +0530

    added Home page routes

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,6 +1,20 @@
-function HomePage(){
+import { Routes, Route } from "react-router-dom";
+import Header from "../components/header";
+
+function HomePage() {
     return (
-        <div>Home Page</div>
+        <div className="w-full h-full">
+            <Header/>
+            <div className="w-full min-h-[calc(100%-100px)]">
+                <Routes>
+                    <Route path="/" element={<h1>Home Page</h1>} />
+                    <Route path="/products" element={<h1>Products Page</h1>} />
+                    <Route path="/about" element={<h1>About Page</h1>} />
+                    <Route path="/contact" element={<h1>Contact Page</h1>} />
+                    <Route path="/*" element={<h1>Page Not Found</h1>} />
+                </Routes>
+            </div>
+        </div>
     );
 }
 export default HomePage;
