850978cf72b371ac50fabfe30520f5f2c9c1ef97
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-01-23 08:21:04 +0530

    added header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,16 @@
-import UserData from "./userData";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo-removebg.png";
 
 function Header(){
     return(
-        <header>
-            
+        <header className="w-full h-[100px] bg-accent flex">
+            <img src={logo} alt="Logo" className="h-full"/>
+            <div className="w-full h-full text-primary text-xl gap-[20px] flex justify-center items-center">
+                <Link to="/" >Home</Link>
+                <Link to="/products" >Products</Link>
+                <Link to="/about" >About</Link>
+                <Link to="/contact" >Contact</Link>
+            </div>
         </header>
     );
 }
