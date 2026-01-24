ac2b1a7b82d0cd56383dfe41262c6fc460bdf6ce
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-01-24 23:24:45 +0530

    start ctreating login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,4 +1,13 @@
 function LoginPage(){
-    return <div>Login Page</div>;
+    return(
+        <div className="w-full h-screen bg-[url('/bg.jpg')] bg-center bg-cover bg-no-repeat ">
+            <div className="w-[50%] h-full ">
+
+            </div>
+            <div className="w-[50%] h-full flex justify-center items-center">
+
+            </div>
+        </div>
+    ); 
 }
 export default LoginPage;
