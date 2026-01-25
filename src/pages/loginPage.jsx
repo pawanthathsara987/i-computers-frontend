75370b9d12b1686bada59594d6ebe751e9497b04
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-01-25 07:14:46 +0530

    login page created

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,13 +1,57 @@
-function LoginPage(){
-    return(
-        <div className="w-full h-screen bg-[url('/bg.jpg')] bg-center bg-cover bg-no-repeat ">
-            <div className="w-[50%] h-full ">
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo-removebg.png';
+import { useState } from 'react';
 
+function LoginPage() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function login(){
+        console.log("Login button clicked");
+        console.log("Email:", email);
+        console.log("Password:", password);
+    }
+
+    return (
+        <div className="w-full h-screen bg-[url('/bg.jpg')] bg-center bg-cover bg-no-repeat flex">
+            <div className="w-[50%] h-full flex justify-center items-center flex-col p-[50px]">
+                <img src={logo} alt="Logo" className="w-[150px] h-[150px] mb-[20px] object-cover" />
+                <h1 className="text-[50px] text-gold text-center text-shadow-accent text-shadow-2xs font-bold">
+                    Plug in. Power Up. Play Hard.
+                </h1>
+                <p className="text-[30px] text-white italic text-center">
+                    Your Ultimate Destination for Gaming Gear
+                </p>
             </div>
             <div className="w-[50%] h-full flex justify-center items-center">
+                <div className="w-[450px] h-[600px] backdrop-blur-lg shadow-2xl rounded-2xl flex flex-col items-center justify-center">
+                    <h1 className='text-[40px] font-bold mb-[20px] text-white text-shadow-white'>Login</h1>
+                    <input
+                        onChange={
+                            (e) => {
+                                setEmail(e.target.value);
+                            }
+                        } type='email' placeholder='your email' className='w-[85%] h-[50px] mb-[20px] rounded-lg border border-accent p-[10px] text-[20px] focus:outline-none focus:ring-1 focus:ring-gold' />
 
+                    <input
+                        onChange={
+                            (e) => {
+                                setPassword(e.target.value);
+                            }
+                        } type='password' placeholder='your password' className='w-[85%] h-[50px] mb-[20px] rounded-lg border border-accent p-[10px] text-[20px] focus:outline-none focus:ring-1 focus:ring-gold' />
+                    <p className='text-white w-[85%] text-right mb-[10px]'>
+                        Forget your password? <Link to="/reset-password" className='text-gold font-bold cursor-pointer'>Reset it here</Link>
+                    </p>
+
+                    <button onClick={login} className='w-[85%] h-[50px] bg-gold text-accent mt-[10px] font-bold text-[20px] rounded-lg hover:bg-accent hover:text-gold transition duration-300'>Login</button>
+
+                    <p className='text-white mt-[10px]'>
+                        Don't have an account? <Link to="/register" className='text-gold font-bold cursor-pointer'>Sign Up</Link>
+                    </p>
+                </div>
             </div>
         </div>
-    ); 
+    );
 }
 export default LoginPage;
