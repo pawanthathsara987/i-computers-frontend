b66def771441250bf15b7a06721d325ba3db7689
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-01-27 00:43:07 +0530

    add product submission functionality with validation and toast notifications

--- src/pages/admin/adminAddProductPage.jsx
+++ src/pages/admin/adminAddProductPage.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineProduct } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 function AdminAddProductPage() {
 
@@ -16,12 +19,59 @@ function AdminAddProductPage() {
     const [model, setModel] = useState("");
     const [stock, setStock] = useState("");
     const [isAvailable, setIsAvailable] = useState(false);
+    const navigate = useNavigate();
 
+    async function addProduct() {
+        const token = localStorage.getItem("token");
+        if (token == null) {
+            toast.error("You must be logged in as admin to add a product.");
+            navigate("/login");
+            return;
+        }
+
+        if (productID == "" || name == "" || description == "" || category == "" || brand == "" || model == "") {
+            toast.error("Please fill in all fields.");
+            return;
+        }
+
+        try {
+
+            const altNamesInArray = altNames.split(",");
+            const imagesInArray = images.split(",");
+
+            await axios.post(import.meta.env.VITE_BACKEND_URL + "/products/", {
+                productID: productID,
+                name: name,
+                altNames: altNamesInArray,
+                description: description,
+                price: price,
+                labelPrice: labelPrice,
+                images: imagesInArray,
+                category: category,
+                brand: brand,
+                model: model,
+                stock: stock,
+                isAvailable: isAvailable
+            }, {
+                headers: {
+                    Authorization: "Bearer " + token
+                }
+            });
+            toast.success("Product added successfully!");
+            navigate("/admin/products");
+
+        } catch (err) {
+            toast.error("Error adding product. Please try again.");
+            console.log("Error adding product:");
+            console.log(err);
+        }
+
+    }
 
     return (
         <div className="w-full h-full flex justify-center p-[50px] items-start overflow-y-scroll">
             <div className="w-[800px] bg-accent/50 rounded-2xl p-[40px]">
-            <h1 className="w-full text-xl text-primary mb-[20px] flex items-center gap-[5px]"><AiOutlineProduct />Add New Product</h1>
+                <h1 className="w-full text-xl text-primary mb-[20px] flex items-center gap-[5px]"><AiOutlineProduct />Add New Product</h1>
                 <div className="w-full bg-white p-[20px] flex flex-row flex-wrap justify-between rounded-xl">
                     <div className="my-[10px] w-[50%]">
                         <label>Product ID</label>
@@ -90,14 +140,13 @@ function AdminAddProductPage() {
                         </select>
                     </div>
                     <div className="my-[10px] w-full flex flex-row flex-wrap justify-between">
-                        <button type="submit" className=" w-[49%] h-[50px] bg-accent text-white px-[40px] py-[10px] border rounded-2xl hover:bg-white hover:text-accent hover:border transition duration-300">
+                        <button onClick={addProduct} type="submit" className=" w-[49%] h-[50px] bg-accent text-white px-[40px] py-[10px] border rounded-2xl hover:bg-white hover:text-accent hover:border transition duration-300">
                             Add Product
                         </button>
                         <Link to="/admin/products" className="w-[49%] h-[50px] bg-red-500 text-white font-bold rounded-2xl flex justify-center items-center hover:bg-red-700 " >
                             Cancel
                         </Link>
                     </div>
-
                 </div>
             </div>
         </div>
