ddf794831fe766b1a167990e6dc09b055ccf98e8
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-01-28 23:58:18 +0530

    add Test component for file upload functionality

---
+++ src/pages/test.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import uploadFile from "../utils/mediaUpload.js";
+
+
+function Test() {
+
+    const [file, setFile] = useState(null);
+
+    async function handleUpload() {
+        const url = await uploadFile(file);
+        console.log(url);
+    }
+
+    return (
+        <div className="w-ful h-full flex  items-center justify-center">
+            <input type="file" onChange={(e) => {
+                console.log(e.target.files);
+                setFile(e.target.files[0]);
+            }} />
+            <button onClick={handleUpload} className="w-[50px] h-[50px] bg-red-900 text-white rounded-2xl m-[25px] py-5 px-[30px] flex items-center justify-center">
+                Upload
+            </button>
+        </div>
+    );
+}
+
+export default Test;
