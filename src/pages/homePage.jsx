03d91154c08d5a526b883b7993be313795044d4a
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-01-31 17:39:57 +0530

    import product page, product overview page and set routes

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "../components/header";
+import ProductPage from "./productPage";
+import ProductOverView from "./productOverView";
 
 function HomePage() {
     return (
@@ -8,9 +10,10 @@ function HomePage() {
             <div className="w-full min-h-[calc(100%-100px)]">
                 <Routes>
                     <Route path="/" element={<h1>Home Page</h1>} />
-                    <Route path="/products" element={<h1>Products Page</h1>} />
+                    <Route path="/products" element={<ProductPage/>} />
                     <Route path="/about" element={<h1>About Page</h1>} />
                     <Route path="/contact" element={<h1>Contact Page</h1>} />
+                    <Route path="/overview/:productID" element={<ProductOverView/>}/>
                     <Route path="/*" element={<h1>Page Not Found</h1>} />
                 </Routes>
             </div>
