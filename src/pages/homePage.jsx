a006f6bfdfe9037150223282188f4a493e5d31d1
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-03 00:47:33 +0530

    import cart and checkout pages and set their Routes

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../components/header";
 import ProductPage from "./productPage";
 import ProductOverView from "./productOverView";
+import CartPage from "./cart";
+import CheckoutPage from "./checkout";
 
 function HomePage() {
     return (
@@ -14,6 +16,8 @@ function HomePage() {
                     <Route path="/about" element={<h1>About Page</h1>} />
                     <Route path="/contact" element={<h1>Contact Page</h1>} />
                     <Route path="/overview/:productID" element={<ProductOverView/>}/>
+                    <Route path="/cart" element={<CartPage/>}/>
+                    <Route path="/checkout"element={<CheckoutPage />} />
                     <Route path="/*" element={<h1>Page Not Found</h1>} />
                 </Routes>
             </div>
