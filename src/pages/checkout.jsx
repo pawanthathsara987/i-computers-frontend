d9649e64119707992ad30092b3f9086ffe28a5c3
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-04 11:02:15 +0530

    add order submission functionality with user input fields

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -6,6 +8,9 @@ export default function CheckoutPage() {
 
     const location = useLocation();
     const navigate = useNavigate();
+    const [name, setName] = useState();
+    const [address, setAddress] = useState();
+    const [phoneNumber, setPhoneNumber] = useState();
 
     const [cart, setCart] = useState(location.state);
 
@@ -13,12 +18,52 @@ export default function CheckoutPage() {
         navigate("/products");
     }
 
-    function getCartTotal(){
+    function getCartTotal() {
         let total = 0;
         cart.forEach((item) => {
             total += item.price * item.quantity
         })
         return total;
+    }
+
+    async function submitOrder() {
+        const token = localStorage.getItem("token");
+
+        if (token == null) {
+            toast.error("You must logged in to place an order");
+            navigate("/login");
+            return;
+        }
+
+        const orderItems = [];
+
+        cart.forEach((item) => {
+            orderItems.push({
+                productID: item.productID,
+                quantity: item.quantity
+            });
+        });
+
+        axios.post(import.meta.env.VITE_BACKEND_URL + "/orders",
+            {
+                name: name,
+                address: address,
+                phoneNumber: phoneNumber,
+                items: orderItems
+            },
+            {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            }
+        ).then(() => {
+            toast.success("Order placed successfully!");
+            navigate("/orders");
+
+        }).catch((error) => {
+            console.log(error.response?.data);
+            toast.error(error.response?.data?.message || "Server error");
+        });
     }
 
     return (
@@ -64,7 +109,7 @@ export default function CheckoutPage() {
                                                 () => {
                                                     const copiedCart = [...cart]
                                                     copiedCart[index].quantity -= 1
-                                                    if(copiedCart[index].quantity< 1){
+                                                    if (copiedCart[index].quantity < 1) {
                                                         copiedCart.splice(index, 1)
                                                     }
                                                     setCart(copiedCart);
@@ -81,8 +126,38 @@ export default function CheckoutPage() {
                     }
                 )
             }
+            <div className="w-[50%] p-4 rounded-xl overflow-hidden shadow-2xl my-1 flex flex-wrap justify-between items-center">
+                <div className="flex flex-col w-[50%] ">
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => { setName(e.target.value) }}
+                        className=" px-6 py-3 rounded border border-secondary/80 focus:border-accent outline-none transition w-[300px]" />
+                </div>
+
+                <div className="flex flex-col w-[50%] ">
+                    <label>Phone</label>
+                    <input
+                        type="text"
+                        value={phoneNumber}
+                        onChange={(e) => { setPhoneNumber(e.target.value) }}
+                        className=" px-6 py-3 rounded border border-secondary/80 focus:border-accent outline-none transition w-[300px]" />
+                </div>
+
+                <div className="flex flex-col w-full ">
+                    <label>Address</label>
+                    <textarea
+                        type="text"
+                        value={address}
+                        onChange={(e) => { setAddress(e.target.value) }}
+                        className=" px-6 py-3 rounded border border-secondary/80 focus:border-accent outline-none transition w-full" />
+                </div>
+            </div>
             <div className="w-[50%] h-[150px] rounded-xl overflow-hidden shadow-2xl my-1 flex justify-between items-center">
-                <button className="self-center ml-4 px-6 py-3 rounded bg-accent text-white hover:bg-accent/90 transition">
+                <button
+                    onClick={submitOrder}
+                    className="self-center ml-4 px-6 py-3 rounded bg-accent text-white hover:bg-accent/90 transition">
                     Order Now
                 </button>
                 <span className="pr-4 text-xl font-bold min-w-[150px] text-right">
