e76465dfbe98b8682bec4a68a8e62d4423425ae9
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-13 06:52:57 +0530

    update test page

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -4,24 +4,32 @@ import uploadFile from "../utils/mediaUpload.js";
 
 function Test() {
 
-    const [file, setFile] = useState(null);
+    // const [file, setFile] = useState(null);
 
-    async function handleUpload() {
-        const url = await uploadFile(file);
-        console.log(url);
-    }
+    // async function handleUpload() {
+    //     const url = await uploadFile(file);
+    //     console.log(url);
+    // }
 
-    return (
-        <div className="w-ful h-full flex  items-center justify-center">
-            <input type="file" onChange={(e) => {
-                console.log(e.target.files);
-                setFile(e.target.files[0]);
-            }} />
-            <button onClick={handleUpload} className="w-[50px] h-[50px] bg-red-900 text-white rounded-2xl m-[25px] py-5 px-[30px] flex items-center justify-center">
-                Upload
-            </button>
+    // return (
+    //     <div className="w-ful h-full flex  items-center justify-center">
+    //         <input type="file" onChange={(e) => {
+    //             console.log(e.target.files);
+    //             setFile(e.target.files[0]);
+    //         }} />
+    //         <button onClick={handleUpload} className="w-[50px] h-[50px] bg-red-900 text-white rounded-2xl m-[25px] py-5 px-[30px] flex items-center justify-center">
+    //             Upload
+    //         </button>
+    //     </div>
+    // );
+
+    return(
+        <div className="w-full h-full bg-red-900 lg:bg-green-900 ">
+
         </div>
     );
+
+
 }
 
 export default Test;
