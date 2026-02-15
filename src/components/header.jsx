de00ec9720e8681258e9fe6ad397926d2515583c
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-15 09:18:06 +0530

    set route UserData

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,12 +3,13 @@ import logo from "../assets/logo-removebg.png";
 import { BiShoppingBag } from "react-icons/bi";
 import { LuListCollapse } from "react-icons/lu";
 import { useState } from "react";
+import UserData from "./userData";
 
 
 function Header() {
     const [sideBarOpen, setSideBarOpen] = useState(false);
     return (
-        <header className="w-full h-[100px] bg-accent flex relative">
+        <header className="w-full h-[100px] bg-accent flex relative items-center">
             <LuListCollapse onClick={() => { setSideBarOpen(true) }} className="text-white my-auto text-2xl ml-6 lg:hidden" />
             <img src={logo} alt="Logo" className="h-full" />
             <div className="w-full h-full hidden lg:flex text-primary text-xl gap-[20px] justify-center items-center">
@@ -17,17 +18,23 @@ function Header() {
                 <Link to="/about" >About</Link>
                 <Link to="/contact" >Contact</Link>
             </div>
-            <Link to="/cart" className="absolute right-4 top=1/2 translate-y-1/2 text-primary text-2xl">
+            <div className="absolute w-[200px] right-15 top=0 h-full justify-center items-center hidden lg:flex">
+                <UserData />
+            </div>
+            <Link to="/cart" className="absolute right-4  text-primary text-2xl">
                 <BiShoppingBag />
             </Link>
             {sideBarOpen && <div className="fixed lg:hidden w-[100nw] h-screen top-0 left-0 bg-black/50 z-20 transition-all duration-300">
                 <div className="bg-white w-[250px] h-screen flex-col relative">
                     <div className="absolute w-full h-full bg-white left-[-250px] translate-x-[250px] transform-flat transition-transform duration-300 flex flex-col">
                         <div className="w-full h-[100px] bg-accent flex justify-center items-center">
-                            <img src="../assets/logo-removebg.png" className="h-full" alt="logo" />
+                            <img src={logo} className="h-full" alt="logo" />
                             <LuListCollapse onClick={() => { setSideBarOpen(false) }} className="text-white my-auto text-2xl ml-6 lg:hidden rotate-180" />
                         </div>
-                        <div className="w-full h-full flex flex-col text-xl text-accent justify-start items-start gap-[50px] ml-[20px] ">
+                        <div className="w-full h-full flex flex-col text-xl text-accent justify-start items-start gap-10 ml-5 mt-10 ">
+                            <div className="p-2 rounded-full flex justify-center bg-accent">
+                                <UserData />
+                            </div>
                             <a className="hover:text-secondary transition" href="/" onClick={() => { setSideBarOpen(false) }}>
                                 Home
                             </a>
