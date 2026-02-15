7cca3529623caea029cf625a0f22862a0221e9f0
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-15 09:16:28 +0530

    update admin page -> check token

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -7,33 +7,67 @@ import AdminProductPage from "./admin/adminProductsPage";
 import AdminAddProductPage from "./admin/adminAddProductPage";
 import AdminUpdateProductPage from "./admin/adminUpdateProductPage";
 import AdminOrdersPage from "./admin/adminOrdersPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../components/loader";
 
 function AdminPage() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if(token ==  null){
+            window.location.href = "/";
+            return;
+        }
+
+        axios.get(import.meta.env.VITE_BACKEND_URL + "/users/",{
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            if(response.data.role == "admin"){
+                setUser(response.data);
+            }else{
+                window.location.href = "/";
+            }
+        }).catch(() => {
+            window.location.href = "/";
+        })
+
+    },[]);
+
     return (
         <div className="w-full h-full flex bg-accent">
-            <div className="w-[300px] bg-accent h-full">
-                <div className="w-full h-[100px] text-primary flex items-center">
-                    <img src={logo} alt="Logo" className="h-full "/>
-                    <h1 className="text-2xl">Admin</h1>
+            {user ?
+            <>
+                <div className="w-[300px] bg-accent h-full">
+                    <div className="w-full h-[100px] text-primary flex items-center">
+                        <img src={logo} alt="Logo" className="h-full " />
+                        <h1 className="text-2xl">Admin</h1>
+                    </div>
+                    <div className="w-full h-[400px] flex flex-col text-2xl text-white pl-[20px] pt-[20px]">
+                        <Link to="/admin" className="w-full flex items-center h-[50px] gap-[10px]"><TbClipboardList />Orders</Link>
+                        <Link to="/admin/products" className="w-full flex items-center h-[50px] gap-[10px]"><LuBoxes />Products</Link>
+                        <Link to="/admin/users" className="w-full flex items-center h-[50px] gap-[10px]"><LuUsers />Users</Link>
+                        <Link to="/admin/reviews" className="w-full flex items-center h-[50px] gap-[10px]"><MdOutlineRateReview />Reviews</Link>
+                    </div>
                 </div>
-                <div className="w-full h-[400px] flex flex-col text-2xl text-white pl-[20px] pt-[20px]">
-                    <Link to="/admin" className="w-full flex items-center h-[50px] gap-[10px]"><TbClipboardList />Orders</Link>
-                    <Link to="/admin/products" className="w-full flex items-center h-[50px] gap-[10px]"><LuBoxes />Products</Link>
-                    <Link to="/admin/users" className="w-full flex items-center h-[50px] gap-[10px]"><LuUsers />Users</Link>
-                    <Link to="/admin/reviews" className="w-full flex items-center h-[50px] gap-[10px]"><MdOutlineRateReview />Reviews</Link>
+                <div className="w-[calc(100%-300px)] max-h-full h-full bg-primary rounded-3xl border-accent border-[2px] overflow-y-scroll ">
+                    <Routes>
+                        <Route path="/" element={<AdminOrdersPage />} />
+                        <Route path="/products" element={<AdminProductPage />} />
+                        <Route path="/add-product" element={<AdminAddProductPage />} />
+                        <Route path="/update-product" element={<AdminUpdateProductPage />} />
+                        <Route path="/users" element={<h1>Users</h1>} />
+                        <Route path="/reviews" element={<h1>Reviews</h1>} />
+                    </Routes>
                 </div>
-            </div>
-            <div className="w-[calc(100%-300px)] max-h-full h-full bg-primary rounded-3xl border-accent border-[2px] overflow-y-scroll ">
-                <Routes>
-                    <Route path="/" element={<AdminOrdersPage />} />
-                    <Route path="/products" element={<AdminProductPage/>} />
-                    <Route path="/add-product" element={<AdminAddProductPage/>} />
-                    <Route path="/update-product" element={<AdminUpdateProductPage/>}/>
-                    <Route path="/users" element={<h1>Users</h1>} />
-                    <Route path="/reviews" element={<h1>Reviews</h1>} />
-                </Routes>
-            </div>
+            </>:
+            <Loader/>
+}
         </div>
+
     );
 }
 export default AdminPage;
