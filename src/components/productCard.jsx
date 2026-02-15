0159df91af01431b4af1d5214076c99bd4cbd4de
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-15 09:19:03 +0530

    update page

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -6,7 +6,7 @@ function ProductCard(props) {
   const product = props.product;
 
   return (
-    <div className="w-[300px] h-[400px] shadow-2xl m-4 cursor-pointer relative hover:[&_.buttons]:opacity-100 hover:[&_.primary-image]:opacity-0">
+    <Link to={"/overview/" + product.productID} className="w-[300px] h-[400px] shadow-2xl m-4 cursor-pointer relative hover:[&_.buttons]:opacity-100 hover:[&_.primary-image]:opacity-0">
       <div className="w-full h-[250px] bg-red-900 relative">
         <img src={product.images[1]} className="w-full h-full absolute bg-white object-cover" />
         <img src={product.images[0]} className="w-full h-full absolute bg-white primary-image transition-opacity duration-500 object-cover" />
@@ -27,9 +27,8 @@ function ProductCard(props) {
       </div>
       <div className="w-full h-[150px] bottom-0 opacity-0 bg-white transition-opacity duration-300 absolute buttons flex flex-row items-center justify-center gap-4">
           <Link to={"/overview/" + product.productID} className=" h-[70px] w-[200px] border-2 border-accent text-accent hover:bg-accent hover:text-white transition-colors duration-150 flex items-center justify-center ">View Details</Link>
-          
       </div>
-    </div>
+    </Link>
   );
 }
 
